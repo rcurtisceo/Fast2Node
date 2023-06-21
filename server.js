853b91937a48d65dc57c13fa9f535660688dc54b
umeraftab7747 <umeraftab7747@gmail.com>
2023-06-21 12:21:49 +0500

    new chnages

--- server.js
+++ server.js
@@ -11,42 +11,46 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+const stripe = require("stripe")(
+  "sk_test_51NJERXGIVwgI2CO5MEddVGtSrjawIPP2YnWE9nEU4lCuHoOFRtjubigWhIA3PYDmKwEDKbL1kvEOd4dvcPuh3qhE00GALjvtVq"
+);
 app.get("/", (req, res) => {
-	res.render("index");
+  res.render("index");
 });
-app.post("/price_set", async (req, res) => {
-	const { price } = req.body;
-	try {
-		const session = await stripe.checkout.sessions.create({
-			payment_method_types: ["card"],
-			mode: "payment",
-			line_items: [
-				{
-					price_data: {
-						currency: `${process.env.currency}`,
-						product_data: {
-							name: "Custom Services",
-						},
-						unit_amount: parseInt(price) * 100,
-					},
-					quantity: 1,
-				},
-			],
+app.get("/price_set/:price", async (req, res) => {
+  const { price } = req.params;
+  console.log(price);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: `USD`,
+            product_data: {
+              name: "Custom Services",
+            },
+            unit_amount: price,
+          },
+          quantity: 1,
+        },
+      ],
 
-			success_url: `${process.env.api_url}/success`,
-			cancel_url: `${process.env.api_url}/cancel`,
-		});
-		res.redirect(session.url);
-	} catch (e) {
-		res.status(500).json({ error: e.message });
-	}
+      success_url: `http://localhost:4000/success`,
+      cancel_url: `http://localhost:4000/cancel`,
+    });
+    res.redirect(session.url);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 app.get("/cancel", (req, res) => {
-	res.render(`cancel`);
+	res.json({ msg: "Cancel" });
 });
 app.get("/success", (req, res) => {
-	res.render(`success`);
+	
+	res.json({ msg: "success" });
 });
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
